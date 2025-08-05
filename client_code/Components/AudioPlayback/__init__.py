f22c0cef5663a2bc39a07139fcb5a41333a11c72
augustincramer.galadrim@gmail.com <augustincramer.galadrim@gmail.com>
2025-08-05 09:03:41 +0000

    Edited Settings.MicrophoneTest, Components.RecordingWidget, Settings.Settings and Components.AudioPlayback

--- client_code/Components/AudioPlayback/__init__.py
+++ client_code/Components/AudioPlayback/__init__.py
@@ -9,6 +9,14 @@ class AudioPlayback(AudioPlaybackTemplate):
     self._audio_blob = None
     self.add_event_handler("x-clear-recording", self.on_clear_recording)
 
+    # Add the 'show' event handler
+    self.add_event_handler("show", self.form_show)
+
+  def form_show(self, **event_args):
+    """This method is called when the component is shown on the screen."""
+    # Explicitly tell the JavaScript to attach its event listeners.
+    anvil.js.call_js("initializeAudioPlayer")
+
   @property
   def audio_blob(self):
     return self._audio_blob
@@ -17,6 +25,7 @@ class AudioPlayback(AudioPlaybackTemplate):
   def audio_blob(self, value):
     self._audio_blob = value
     if self.parent:
+      # This part is correct and sets the audio source.
       anvil.js.call_js("setupAudioPlayback", value)
 
   def on_clear_recording(self, **event_args):
--- client_code/Settings/MicrophoneTest/__init__.py
+++ client_code/Settings/MicrophoneTest/__init__.py
@@ -6,31 +6,52 @@ import anvil.users
 
 class MicrophoneTest(MicrophoneTestTemplate):
   def __init__(self, **properties):
-    # Connect the user using the standard login form
-    anvil.users.login_with_form()
     self.init_components(**properties)
+
+    # 1. Set up the event handlers for our components
     self.recording_widget_1.set_event_handler(
-      "recording_complete", self.handle_test_recording
+      "recording_complete", self.handle_recording_complete
     )
+    self.audio_playback_1.set_event_handler(
+      "x-clear-recording", self.reset_to_record_mode
+    )
+
+    # 2. Set the initial state of the form when it loads
     self.add_event_handler("show", self.form_show)
 
   def form_show(self, **event_args):
-    print("Formulaire MicrophoneTest affiché.")
+    """
+    This method is called when the form is first displayed.
+    It sets the initial UI state to "record mode".
+    """
+    self.reset_to_record_mode()
 
-  # Relay function to show errors.
-  def show_error(self, error_message, **event_args):
-    alert(error_message)
+  def handle_recording_complete(self, audio_blob, **event_args):
+    """
+    This method is called by the RecordingWidget when a recording is finished.
+    It switches the UI to "playback mode".
+    """
+    print("Recording complete. Switching to playback mode.")
 
-  def handle_test_recording(self, audio_blob, **event_args):
-    """This event handler is called by the RecordingWidget."""
-    self.process_test_recording(audio_blob)
+    # 1. Make the recording widget invisible
+    self.recording_widget_1.visible = False
 
-  def process_test_recording(self, audio_blob, **event_args):
-    print("process_test_recording() appelé avec un blob audio de test.")
-    try:
-      transcription = anvil.server.call("process_and_log_test", audio_blob)
-      print("Transcription renvoyée :", transcription)
-      open_form("Settings.Settings")
-      return transcription
-    except Exception as e:
-      alert("Erreur lors du traitement de l'enregistrement de test : " + str(e))
+    # 2. Pass the recorded audio to the playback component
+    self.audio_playback_1.audio_blob = audio_blob
+
+    # 3. Make the playback component visible
+    self.audio_playback_1.visible = True
+
+  def reset_to_record_mode(self, **event_args):
+    """
+    This method is called by the AudioPlayback component when its 'clear'
+    button is clicked. It resets the UI to its initial "record mode".
+    """
+    print("Resetting to record mode.")
+
+    # 1. Make the playback component invisible and clear its audio
+    self.audio_playback_1.visible = False
+    self.audio_playback_1.audio_blob = None
+
+    # 2. Make the recording widget visible again
+    self.recording_widget_1.visible = True
